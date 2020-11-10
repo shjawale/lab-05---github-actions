ccf788d33c094068196c8eb2f5e7e0e06d7efd09
shjawale <shruti.s.jawale@gmail.com>
2020-11-09 23:21:18 -0800

    Add rectangle.cpp

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,1 +1,27 @@
+#include "../header/rectangle.hpp"
 
+Rectangle::Rectangle(int w, int h) : width(w), height(h) {}
+
+void Rectangle::set_width(int w){
+	this->width = w;
+}
+
+void Rectangle::set_height(int h){
+	this->height = h;
+}
+
+int Rectangle::get_width(){
+    return width;
+}
+
+int Rectangle::get_height(){
+    return height;
+}
+
+int Rectangle::area(){
+    return -1;
+}
+
+int Rectangle::perimeter(){
+    return -1;
+}
